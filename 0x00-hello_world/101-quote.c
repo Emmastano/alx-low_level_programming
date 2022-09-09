7809a1128c84f4f40fbc4ee4ff713ce8780ccf99
Emmastano <emmastano84@gmail.com>
2022-09-09 04:41:53 +0000

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 
 /**
- * main- entry point and print the required output for the peogram
+ * main- entry point and print the required output for the program
  *
  * Return: the value of 1 always
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	write(2 "and that piece of art is useful\" - Dora Korpar,2015-10-19\n", 59);
+	return (0);
 }

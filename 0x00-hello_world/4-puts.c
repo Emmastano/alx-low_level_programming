82a9a662bd7c35861834790e78f84df653563235
Emmastano <emmastano84@gmail.com>
2022-09-09 03:08:54 +0000

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- * 
- * Return: Always 0 (Success)
+ * main-Enry point
+ *
+ * Return:Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
+	put("\"Programming is like building a multilingual puzzle");
+	reture (0)
 }

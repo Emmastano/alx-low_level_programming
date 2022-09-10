1f1a51ba1651af67a5f416b7f773eece915ac2f3
Emmastano <emmastano84@gmail.com>
2022-09-10 06:28:47 +0000

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point and print all possible different conbinations of two
+ *        digits, separated by a comma and followed by a space, in ascending
+ *        order.
+ *
+ * Return: the value is 0 always.
+ */
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 < 10; digit1++)
+	{
+		for (digit2 = 0; digit2 < 10; digit2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}

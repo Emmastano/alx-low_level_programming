fe48dc1a62a51fc14686c3ac787df8edb6c2e95d
Emmastano <emmastano84@gmail.com>
2022-09-14 02:18:00 +0000

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - print_alphabet in lowercase.
+ *
+ * Return: the value of 0 always
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+}

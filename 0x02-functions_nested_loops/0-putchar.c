add8167522cbca8276d9d1c08735b60819d8e71f
Emmastano <emmastano84@gmail.com>
2022-09-14 02:07:37 +0000

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*main - Entry point and print prints _putchar, followed by a new line.
+*
+*Return: the value of 0 always
+*/
+
+int main(void)
+
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);

09d53a5978a37c6ed6fb6ef6b18e5080a12463d9
Emmastano <emmastano84@gmail.com>
2022-09-19 13:03:13 +0000

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _3-puts -  function that prints a string, followed by a new line, to stdout
+ * @str: string to print
+ * Reurn: always 0
+ */
+
+void_puts(char *str)
+{
+	int 1 = 0;
+
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i++;
+	}
+
+	putchar('\n')
+}
